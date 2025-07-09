342e176db76a65056d84bab1c19a2e6d3d795627
renawmontanari <regiangelica@gmail.com>
2025-07-09 17:29:14 -0300

    ajustando o componente card

--- src/components/CardCategorie.vue
+++ src/components/CardCategorie.vue
@@ -12,12 +12,52 @@ export default {
 <template>
   <article class="categorie">
     <header class="categorie__header">
-      <h2 class="paragrafo-lg categoria__nome">{{ categorie.genre }}</h2>
+      <h2 class="subtitle-lg categorie__genre">{{ categorie.genre }}</h2>
     </header>
-    <ul class="categoria__ingredientes">
+    <ul class="categorie__information">
       <li v-for="categorie in categorie.themes" :key="categorie">
         {{ categorie }}
       </li>
     </ul>
   </article>
 </template>
+
+<style scoped>
+.categorie {
+  width: 19.5rem;
+  padding: 1rem;
+  border-radius: 1rem;
+  background: var(--branco, #fff);
+  box-shadow: 4px 4px 10px 0px rgba(68, 68, 68, 0.05);
+  height: 100%;
+
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 2rem;
+}
+
+.categorie__header {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 0.5rem;
+}
+
+/* .categoria__imagem {
+  width: 3.5rem;
+} */
+
+.categorie__genre {
+  text-align: center;
+  color: var(--text-primary, #6d214f);
+  font-weight: 700;
+}
+
+.categorie__information {
+  display: flex;
+  justify-content: center;
+  gap: 0.5rem;
+  flex-wrap: wrap;
+}
+</style>
