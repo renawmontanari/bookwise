46776bd23ef2823ed2f8194d31f52137d6b5505f
renawmontanari <regiangelica@gmail.com>
2025-08-07 21:08:37 -0300

    ajuste no footer

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -9,7 +9,7 @@ export default {
     <div class="footer-content">
       <div class="footer-left">
         <p>Feito por Renan Dev</p>
-        <img src="/public/renandev.png" alt="Renan Dev" />
+        <img class="imgDev" src="/public/renandev.png" alt="Renan Dev" />
       </div>
       <div>
         <p>&copy; 2025 Bookwise. Todos os direitos reservados.</p>
@@ -28,5 +28,16 @@ export default {
 
 .footer-left {
   display: flex;
+  padding: 4px;
+  color: #ccc;
+  gap: 8px;
+  background-color: rgb(51, 51, 51);
+  border-radius: 10px;
+  align-items: center;
+  border: solid 1px #ccc;
+}
+
+.imgDev {
+  border-radius: 50%;
 }
 </style>
